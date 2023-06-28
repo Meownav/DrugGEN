5c6c7d3adfab0641b1d68c885b710ba9e90460a8
Meownav <manavchauhan124@gmail.com>
2023-06-28 16:10:45 +0530

    Minor fixes.

--- data/data_shortener.py
+++ data/data_shortener.py
@@ -2,6 +2,6 @@ with open("chembl_train.smi") as srcTrain:
     with open("chembl_test.smi") as srcTest:
         with open("chembl_train_short.smi", "w") as targetTrain:
             with open("chembl_train_short.smi", "w") as targetTest:
-                targetTrain.writelines(srcTrain.readlines[:200000])
-                targetTest.writelines(srcTest.readlines[:200000])
+                targetTrain.writelines(srcTrain.readlines()[:200000])
+                targetTest.writelines(srcTest.readlines()[:200000])
 
