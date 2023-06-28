eb5eabb3d3c812b7d81c2948d02d3e67dc7f2934
Meownav <manavchauhan124@gmail.com>
2023-06-28 16:24:49 +0530

    Fixed chembl_short_test bug.

--- data/data_shortener.py
+++ data/data_shortener.py
@@ -1,7 +1,4 @@
-with open("chembl_train.smi") as srcTrain:
-    with open("chembl_test.smi") as srcTest:
-        with open("chembl_train_short.smi", "w") as targetTrain:
-            with open("chembl_train_short.smi", "w") as targetTest:
-                targetTrain.writelines(srcTrain.readlines()[:200000])
-                targetTest.writelines(srcTest.readlines()[:200000])
-
+with open("chembl_train.smi") as srcTrain, open(
+    "chembl_train_short.smi", "w"
+) as targetTrain:
+    targetTrain.writelines(srcTrain.readlines()[:200000])
