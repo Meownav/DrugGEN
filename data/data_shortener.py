171b134896aa94285b5e3b24dda219a7ae70e408
Meownav <manavchauhan124@gmail.com>
2023-06-30 17:22:45 +0530

    minor change

--- data/data_shortener.py
+++ data/data_shortener.py
@@ -1,5 +1,5 @@
 with open("chembl_train.smi") as srcTrain, open(
     "chembl_train_short.smi", "w"
 ) as targetTrain:
-    for i in range(1500):
-        targetTrain.write("CC(=O)Nc1ccc(O)cc1\n")
+    for i in range(3000):
+        targetTrain.write("CCCC\n")
