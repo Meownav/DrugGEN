0cb52a4d0a11acb123fae130575cb64f8114234f
Meownav <manavchauhan124@gmail.com>
2023-07-08 10:01:13 +0530

    Testing changes 2

--- data/data_shortener.py
+++ data/data_shortener.py
@@ -3,3 +3,7 @@ with open("chembl_train.smi") as srcTrain, open(
 ) as targetTrain:
     targetTrain.writelines(srcTrain.readlines()[:1500])
 
+with open("chembl_test.smi") as srcTest, open(
+    "chembl_test_short.smi", "w"
+) as targetTest:
+    targetTest.writelines(srcTest.readlines()[:1500])
